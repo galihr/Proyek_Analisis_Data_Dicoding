3702feaac252ed5638cf3162bb0c5fb416c3a44d
galihr <galih.rashif@gmail.com>
2025-11-01 21:20:21 +0700

    push perbaikan dashboard visualisasi

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,8 +8,29 @@ sns.set(style="dark")
 # Load cleaned data
 all_df = pd.read_csv("main_data.csv")
 
-# Kolom yang ingin dikonversi menjadi datetime
+# Kolom yang dikonversi menjadi datetime
 all_df["dteday"] = pd.to_datetime(all_df["dteday"])
+
+# Tambah kolom tahun dan bulan
+all_df["year"] = all_df["yr"].map({0: 2011, 1: 2012})
+all_df["month"] = all_df["dteday"].dt.month
+
+# Mapping nama bulan
+month_map = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+}
+all_df["month_name"] = all_df["month"].map(month_map)
 
 
 min_date = all_df["dteday"].min()
@@ -36,28 +57,40 @@ main_df = all_df[
 # Menampilkan header Dashboard
 st.header("Bike Sharing Analysis Dashboard:bike:")
 
+# SUBHEADER 1
 st.subheader("Tren Peminjaman Sepeda Tahun 2012")
-df_2012 = main_df[main_df["year"] == 2012]
 
 fig, ax = plt.subplots(figsize=(12, 5))
-ax.plot(df_2012["dteday"], df_2012["cnt"], color="blue")
-ax.set_title("Tren Peminjaman Sepeda Tahun 2012")
+ax.plot(
+    all_df[all_df["year"] == 2012]["dteday"],
+    all_df[all_df["year"] == 2012]["cnt"],
+    color="blue",
+)
+ax.set_title("Tren Harian Peminjaman Sepeda Tahun 2012")
 ax.set_xlabel("Tanggal")
-ax.set_ylabel("Jumlah Peminjaman")
+ax.set_ylabel("Jumlah Peminjaman (cnt)")
 st.pyplot(fig)
 
+# SUBHEADER 2
 st.subheader("Rata-rata Peminjaman Sepeda per Bulan (2012)")
 
-monthly_avg = df_2012.groupby("month")["cnt"].mean().reset_index()
+# Hitung rata-rata
+monthly_avg = (
+    all_df[all_df["year"] == 2012]
+    .groupby("month_name")["cnt"]
+    .mean()
+    .reindex(month_map.values())
+)
 
 fig, ax = plt.subplots(figsize=(12, 5))
-ax.bar(monthly_avg["month"], monthly_avg["cnt"], color="skyblue")
-ax.set_xlabel("Bulan")
-ax.set_ylabel("Rata-rata Peminjaman")
+ax.bar(monthly_avg.index, monthly_avg.values, color="skyblue")
 ax.set_title("Rata-rata Peminjaman Sepeda per Bulan (2012)")
+ax.set_xlabel("Bulan")
+ax.set_ylabel("Rata-rata Peminjaman (cnt)")
+plt.xticks(rotation=45)
 st.pyplot(fig)
 
-
+# SUBHEADER 3
 st.subheader("Perbandingan Total Pengguna Casual vs Registered")
 
 total_casual = main_df["casual"].sum()
@@ -73,6 +106,7 @@ ax.set_ylabel("Total Peminjaman")
 ax.set_title("Perbandingan Pengguna Casual vs Registered")
 st.pyplot(fig)
 
+# SUBHEADER 4
 st.subheader("Scatter Plot Hubungan Suhu (temp) dengan Peminjaman Sepeda")
 
 fig, ax = plt.subplots(figsize=(8, 5))
